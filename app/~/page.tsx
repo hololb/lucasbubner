a7f233946ef8efbbb6425aaec433a3a2d6c12130
Lucas Bubner <lkbubner@proton.me>
2024-07-14 00:12:29 +0930

    Use fragments instead of an unnecessary span

--- app/~/page.tsx
+++ app/~/page.tsx
@@ -43,60 +43,60 @@ export default function Scrollway() {
 
     const comments: JSX.Element[][] = [
         [
-            <span key={0}>
+            <>
                 I have a strong <span className="__text-emp-red">passion</span> for technology and software.
-            </span>,
-            <span key={1}>
+            </>,
+            <>
                 I build and develop to <span className="__text-emp-red">solve</span> problems.
-            </span>,
-            <span key={2}>
+            </>,
+            <>
                 I have a <span className="__text-emp-red">dedicated</span> work ethic.
-            </span>,
-            <span key={3}>
+            </>,
+            <>
                 I ensure my projects are of only the <span className="__text-emp-red">highest</span> standards.
-            </span>,
-            <span key={4}>
+            </>,
+            <>
                 I have a strong sense of <span className="__text-emp-red">responsibility</span> and always strive to{" "}
                 <span className="__text-emp-red">exceed expectations</span>.
-            </span>,
+            </>,
         ],
         [
-            <span key={5}>
+            <>
                 I have skills with a <span className="__text-emp-red">wide range</span> of hardware and software tools.
-            </span>,
-            <span key={6}>
+            </>,
+            <>
                 I have experience communicating <span className="__text-emp-red">complex ideas</span> in an{" "}
                 <span className="__text-emp-red">innovative</span> way.
-            </span>,
-            <span key={7}>
+            </>,
+            <>
                 I captain the Murray Bridge High School <span className="__text-emp-red">Student Robotics Club</span>.
-            </span>,
-            <span key={8}>
+            </>,
+            <>
                 I am a <span className="__text-emp-red">nationally recognised</span> winner at the AIIA iAwards 2023.
-            </span>,
-            <span key={9}>
+            </>,
+            <>
                 I create intellectual property to <span className="__text-emp-red">improve and provide</span>.
-            </span>,
+            </>,
         ],
         [
-            <span key={10}>
+            <>
                 I have experience in <span className="__text-emp-red">project management</span> and{" "}
                 <span className="__text-emp-red">delivering on time</span>.
-            </span>,
-            <span key={11}>
+            </>,
+            <>
                 I follow <span className="__text-emp-red">industry standards</span> and{" "}
                 <span className="__text-emp-red">best practices</span>.
-            </span>,
-            <span key={12}>
+            </>,
+            <>
                 I desire to <span className="__text-emp-red">positively impact</span> the world through technology.
-            </span>,
-            <span key={13}>
+            </>,
+            <>
                 I am a <span className="__text-emp-red">high-achieving</span> academic student.
-            </span>,
-            <span key={14}>
+            </>,
+            <>
                 I am a quick learner and <span className="__text-emp-red">adaptable</span> to new technologies and
                 environments.
-            </span>,
+            </>,
         ],
         [
             <span key={15}>
